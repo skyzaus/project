335cea3c10228420c1e127785765d9cc47c57001
Sky Rader <sky_rader1@hotmail.com>
2023-12-30 00:13:07 -0800

    updated JS

---
+++ app.js
@@ -0,0 +1,18 @@
+function randomRBG(){
+const r = Math.floor(Math.random()* 256);
+const g = Math.floor(Math.random()* 256);
+const b = Math.floor(Math.random()* 256);
+return `rgb(${r},${g},${b})`
+console.log()
+}
+
+const letters = document.querySelectorAll('.letter');
+const intervalID = setInterval(function () {
+for (let letter of letters){
+    letter.style.color = randomRBG();
+console.log ()
+}
+
+
+
+}, 2000)
